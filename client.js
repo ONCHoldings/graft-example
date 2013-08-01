5391880fd7807e7bd62c483bd1ce612db39af5e3
Adrian Rossouw <adrian@daemon.co.za>
2013-08-01 09:40:09 +0200

    got the client side working again

--- client.js
+++ client.js
@@ -7,13 +7,17 @@ _  = require('underscore');
 Graft.addInitializer(function(options) {
     require('./models/Progress.graft.js');
     require('./views/Progress.graft.js');
-    require('./views/Login.graft.js');
 });
 
 /**
  * Backbone.js client side initialization.
  */
 Graft.on('start', function(options) {
+    // Manage the initial regions
+    this.addRegions({
+        sidebar: '#sidebar',
+        content: '#content'
+    });
 
     Graft.$state.progress = new Graft.$models.Progress({progress: 10});
     function timer() {
@@ -32,7 +36,7 @@ Graft.on('start', function(options) {
     }));
 
     this.$state.progress.on('complete', function() {
-        this.content.show(new Graft.$views.Account());
+        //this.content.show(new Graft.$views.Account());
     }, this);
 
     Backbone.history.start({push$state: true, silent: false, root: "/"});
