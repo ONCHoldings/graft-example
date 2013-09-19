5e6927cd4b6b6c43b9199fc638b2915961c06db5
Adrian Rossouw <adrian@daemon.co.za>
2013-09-19 10:02:32 +0200

    port to bootstrap-3

--- views/progress.js
+++ views/progress.js
@@ -1,13 +1,15 @@
 module.exports = Backbone.Marionette.ItemView.extend({
-    className: 'progress-bar',
+    className: 'progress',
     template: require('../templates/progress-bar.jade'),
     modelEvents: {
         'change:progress' : 'progress'
     },
     ui: {
-        'bar': '.progress .bar'
+        'bar': '.progress-bar',
+        'sr' : '.progress-bar .sr-only'
     },
     progress: function() {
         this.ui.bar.get(0).style.width = this.model.get('progress') + '%';
+        this.ui.sr.attr('aria-valuenow', '' + this.model.get('progress'));
     }
 });
